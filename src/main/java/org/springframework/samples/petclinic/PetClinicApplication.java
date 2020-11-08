32a0d63d1860cf95b88ed1c0657ed910ea73ae25
dpavlov <dpavlov@iitdgroup.ru>
2020-11-08 17:05:41 +0700

    Code change for testing team city build.

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -28,6 +28,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication(proxyBeanMethods = false)
 public class PetClinicApplication {
 
+	// Comment for testing Team City build.
 	public static void main(String[] args) {
 		SpringApplication.run(PetClinicApplication.class, args);
 	}
