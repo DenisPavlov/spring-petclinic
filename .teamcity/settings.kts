1177bf2d6319aafe237655e5874190ba0777f590
dpavlov <dpavlov@iitdgroup.ru>
2020-11-08 19:30:48 +0700

    Name and Id example

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -28,7 +28,7 @@ To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
 version = "2020.1"
 
 project {
-    vcsRoot(PetclinicVcs)
+//    vcsRoot(PetclinicVcs)
     buildType(Build)
 }
 
@@ -38,7 +38,7 @@ object Build : BuildType({
     artifactRules = "target/*jar"
 
     vcs {
-        root(PetclinicVcs)
+//        root(PetclinicVcs)
     }
 
     steps {
@@ -55,10 +55,10 @@ object Build : BuildType({
     }
 })
 
-object PetclinicVcs : GitVcsRoot({
-    name = "PetclinicVcs"
-    url = "https://github.com/DenisPavlov/spring-petclinic.git"
-})
+//object PetclinicVcs : GitVcsRoot({
+//    name = "PetclinicVcs"
+//    url = "https://github.com/DenisPavlov/spring-petclinic.git"
+//})
 
 //fun wrapWithFeature(buildType: BuildType, featureBlock: BuildFeatures.() -> Unit): BuildType {
 //    buildType.features {
